4df84e8aba5175b4a2ccce7113cfec9456ffc095
saloni369 <133067220+saloni369@users.noreply.github.com>
2024-09-25 17:36:03 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,6 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    function isValid(s) {
+        let stack = [];
+        let map = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        };
+    
+        for (let i = 0; i < s.length; i++) {
+            let char = s[i];
+    
+            if (map[char]) {
+                stack.push(char);
+            } else {
+                let last = stack.pop();
+                if (map[last] !== char) {
+                    return false;
+                }
+            }
+        }
+    
+        return stack.length === 0;
+    }
+    
+    console.log(isValid("()"));        // Output: true
+    console.log(isValid("()[]{}"));    // Output: true
+    console.log(isValid("(]"));        // Output: false
+    
     
 };
 
